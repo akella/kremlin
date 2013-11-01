2c6694d912ecfbc29ef1f5fc75917fadb76a0cdd
akella <akella.a@gmail.com>
2013-11-01 10:56:01 +0200

    fix headers, clarify issues

--- dev/js/common.js
+++ dev/js/common.js
@@ -190,4 +190,33 @@ $(document).ready(function() {
 		setpopup();
 	});
 
+
+
+
+	var stickyPanelSettings = {
+        // Use this to set the top margin of the detached panel.
+        topPadding: 60,
+
+        // This class is applied when the panel detaches.
+        afterDetachCSSClass: "",
+
+        // When set to true the space where the panel was is kept open.
+        savePanelSpace: true,
+
+        // Event fires when panel is detached
+        // function(detachedPanel, panelSpacer){....}
+        onDetached: null,
+
+        // Event fires when panel is reattached
+        // function(detachedPanel){....}
+        onReAttached: null,
+
+        // Set this using any valid jquery selector to 
+        // set the parent of the sticky panel.
+        // If set to null then the window object will be used.
+        parentSelector: null
+    };
+    $(".fixme").stickyPanel(stickyPanelSettings);
+
+
 });
